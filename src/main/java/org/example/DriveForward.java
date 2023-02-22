b2124afe4622d631ddaf8a38edee93aff7ac8533
Alex Batten <alex.batten@live.com>
2023-02-22 11:24:36 +0100

    Added starting behaivour code

---
+++ src/main/java/org/example/DriveForward.java
@@ -0,0 +1,42 @@
+package org.example;
+
+import lejos.remote.ev3.RMIRegulatedMotor;
+import lejos.robotics.subsumption.Behavior;
+
+import java.rmi.RemoteException;
+
+public class DriveForward implements Behavior {
+
+    RMIRegulatedMotor harvester;
+    RMIRegulatedMotor a;
+    RMIRegulatedMotor b;
+
+    public DriveForward(RMIRegulatedMotor harvester, RMIRegulatedMotor a, RMIRegulatedMotor b) {
+        this.harvester = harvester;
+        this.a = a;
+        this.b = b;
+
+    }
+
+
+    @Override
+    public boolean takeControl() {
+        return false;
+    }
+
+    @Override
+    public void action() {
+        try {
+            harvester.forward();
+            a.forward();
+            b.backward();
+        } catch (RemoteException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void suppress() {
+
+    }
+}
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,11 +3,12 @@ package org.example;
 import lejos.hardware.Audio;
 import lejos.remote.ev3.RMIRegulatedMotor;
 import lejos.remote.ev3.RemoteEV3;
+import lejos.robotics.subsumption.Arbitrator;
+import lejos.robotics.subsumption.Behavior;
 
 public class Main {
     public static void main(String[] args) {
         try{
-        System.out.println("poopoo");
         RemoteEV3 ev3 = new RemoteEV3("172.20.10.8");
         ev3.setDefault();
         Audio sound = ev3.getAudio();
@@ -15,7 +16,6 @@ public class Main {
         sound.playTone(220, 1500);
         sound.systemSound(0);
         System.out.println("Når vi her?");
-
         RMIRegulatedMotor a = ev3.createRegulatedMotor("A", 'L');
         RMIRegulatedMotor b =ev3.createRegulatedMotor("D", 'L');
         RMIRegulatedMotor harvester =ev3.createRegulatedMotor("B", 'M');
@@ -27,9 +27,14 @@ public class Main {
         a.setSpeed(720);
         b.setSpeed(720);
         System.out.println("set speed");
-        harvester.forward();
-        a.forward();
-        b.backward();
+
+        // behaviour
+
+        Behavior b1 = new DriveForward(harvester, a, b);
+        Behavior [] bArray = {b1};
+        Arbitrator arby = new Arbitrator(bArray);
+        arby.go();
+
         System.out.println("startet begge motorer");
         try {
             Thread.sleep(10000);
