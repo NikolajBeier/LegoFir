237e8656ecfa8aa86bdd27fa97d5dcae952f1e27
nikolajbeier <s215814@dtu.dk>
2023-05-31 14:40:32 +0200

    Made proper edge detection. TODO: Tweak the values so theres only 4 lines, make intersections and integrate the intersection points to the perspective transform code.

--- src/main/java/org/example/camera/EdgeDetection.java
+++ src/main/java/org/example/camera/EdgeDetection.java
@@ -3,8 +3,12 @@ package org.example.camera;
 import org.example.mapping.ObjectColor;
 import org.example.robot.model.Legofir;
 import org.opencv.core.*;
+import org.opencv.core.Point;
+import org.opencv.highgui.HighGui;
+import org.opencv.imgcodecs.Imgcodecs;
 import org.opencv.imgproc.Imgproc;
 
+import java.awt.*;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -60,4 +64,51 @@ public class EdgeDetection {
         return null;
     }
 
+
+
+    public Point[] intersectionDetect(Mat image) {
+        //Imgproc.cvtColor(image, hlsimage, Imgproc.COLOR_BGR2HLS);
+        //Imgcodecs.imwrite("beforebefore.jpg", hlsimage);
+        Core.inRange(image, new Scalar(0, 0, 150), new Scalar(160, 105, 255), redMask);
+
+        Imgcodecs.imwrite("before.jpg", redMask);
+
+
+        // Canny Edge Detection
+        //Mat edges = new Mat();
+        //Imgproc.Canny(redMask, edges, 50, 100, 7, false);
+        //Imgcodecs.imwrite("canny.jpg", edges);
+
+        // Apply Hough Line Transform
+        Mat lines = new Mat();
+        Imgproc.HoughLinesP(redMask, lines, 1, Math.PI / 180, 250, 10, 5);
+
+        // Draw the lines
+        Mat houghLines = new Mat();
+        houghLines.create(redMask.rows(), redMask.cols(), CvType.CV_8UC1);
+
+        Imgcodecs.imwrite("weirdone.jpg", houghLines);
+
+        // This will ensure only the top 4 lines are processed
+        int limit = 20;
+        for (int i = 0; i < lines.rows() && i < limit; i++) {
+            double[] points = lines.get(i, 0);
+            double x1, y1, x2, y2;
+            x1 = points[0];
+            y1 = points[1];
+            x2 = points[2];
+            y2 = points[3];
+
+            Point pt1 = new Point(x1, y1);
+            Point pt2 = new Point(x2, y2);
+
+            // Draw line on the image
+            Imgproc.line(image, pt1, pt2, new Scalar(255, 0, 0), 5);
+        }
+
+        // Save the output
+        Imgcodecs.imwrite("houghlines.jpg", houghLines);
+
+        return null;
+    }
 }
