bb0016a7e3dd8ec495dbb26571506d55e082049f
marcusjacobsen <S215799@dtu.dk>
2023-06-05 14:06:59 +0200

    fatal crash

--- src/main/java/org/example/camera/ObstacleDetection.java
+++ src/main/java/org/example/camera/ObstacleDetection.java
@@ -24,9 +24,25 @@ public class ObstacleDetection {
     int lMax = ObjectColor.getEdge().getValMax();
     int offsetY= 120;
     int offsetX= 400;
+    Point pt1;
+    Point pt2;
+    List<Point> points = new ArrayList<>();
+    Point topPoint = new Point(0,10000);
+    Point bottomPoint = new Point(0,0);
+    Point leftPoint = new Point(10000,0);
+    Point rightPoint = new Point(0,0);
+
+    Point robotTopPoint= new Point(0,0);
+    Point robotBottomPoint= new Point(0,0);
+    Point robotLeftPoint= new Point(0,0);
+    Point robotRightPoint= new Point(0,0);
+    Mat lines = new Mat();
+
     public ObstacleDetection() {}
 
-    public Mat detect(Mat image, Point topObstacle, Point bottomObstacle, Point leftObstacle, Point rightObstacle, Legofir dude) {
+
+
+    public void detect(Mat image, Point topObstacle, Point bottomObstacle, Point leftObstacle, Point rightObstacle, Legofir dude) {
 
         Core.inRange(image, new Scalar(hMin, sMin, lMin), new Scalar(hMax, sMax, lMax), redMask);
 
@@ -34,22 +50,23 @@ public class ObstacleDetection {
         croppedMask=redMask.colRange(new Range(offsetX,image.width()-offsetX)).rowRange(new Range(offsetY,image.height()-offsetY));
 
         // Apply Hough Line Transform
-        Mat lines = new Mat();
         Imgproc.HoughLinesP(croppedMask, lines, 1, Math.PI / 180, 80 , 30, 10);
 
-        List<Point> points = new ArrayList<>();
+        points.clear();
         for( int i = 0; i < lines.rows(); i++ )
         {
             double[] data = lines.get(i, 0);
-            Point pt1 = new Point(data[0]+offsetX, data[1]+offsetY);
-            Point pt2 = new Point(data[2]+offsetX, data[3]+offsetY);
+            pt1 = new Point(data[0]+offsetX, data[1]+offsetY);
+            pt2 = new Point(data[2]+offsetX, data[3]+offsetY);
             points.add(pt1);
             points.add(pt2);
         }
-        Point topPoint = new Point(0,10000);
-        Point bottomPoint = new Point(0,0);
-        Point leftPoint = new Point(10000,0);
-        Point rightPoint = new Point(0,0);
+
+        topPoint.set(new double[]{0,10000});
+        bottomPoint.set(new double[]{0,0});
+        leftPoint.set(new double[]{10000,0});
+        rightPoint.set(new double[]{0,0});
+
         for(Point point : points){
             if(point.y < topPoint.y){
                 topPoint = point;
@@ -65,15 +82,16 @@ public class ObstacleDetection {
             }
         }
 
-        Point robotTopPoint= new Point(topPoint.x, -topPoint.y);
-        Point robotBottomPoint= new Point(bottomPoint.x, -bottomPoint.y);
-        Point robotLeftPoint= new Point(leftPoint.x, -leftPoint.y);
-        Point robotRightPoint= new Point(rightPoint.x, -rightPoint.y);
+        robotTopPoint.x = topPoint.x;
+        robotTopPoint.y = -topPoint.y;
+        robotBottomPoint.x = bottomPoint.x;
+        robotBottomPoint.y = -bottomPoint.y;
+        robotLeftPoint.x = leftPoint.x;
+        robotLeftPoint.y = -leftPoint.y;
+        robotRightPoint.x = rightPoint.x;
+        robotRightPoint.y = -rightPoint.y;
 
         dude.getMap().setObstacle(robotTopPoint, robotBottomPoint, robotLeftPoint, robotRightPoint);
-
-
-        return image;
     }
     private void addLine(double rho, double theta, List<Double> rhoList, List<Line2D> edges, Mat image){
         double a = cos(theta);
